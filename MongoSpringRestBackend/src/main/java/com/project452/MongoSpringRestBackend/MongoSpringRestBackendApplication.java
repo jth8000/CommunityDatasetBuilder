29413690203b690ed3dddd05b76834988a7d682e
Jacob Harris <jth8000@gmail.com>
2021-06-09 16:47:07 -0600

    Initialized Spring project for NoSQL backend

---
+++ MongoSpringRestBackend/src/main/java/com/project452/MongoSpringRestBackend/MongoSpringRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project452.MongoSpringRestBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSpringRestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSpringRestBackendApplication.class, args);
+	}
+
+}
---
+++ MongoSpringRestBackend/src/test/java/com/project452/MongoSpringRestBackend/MongoSpringRestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project452.MongoSpringRestBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSpringRestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
